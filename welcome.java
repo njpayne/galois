173d303d19f0c898a4597b80a8ebab5f9b904db1
Nathaniel Payne <njpayne@sfu.ca>
2013-09-14 00:28:55 -0700

    Hello World

---
+++ welcome.java
@@ -0,0 +1,10 @@
+/**
+ * The welcome class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+public class welcome {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
