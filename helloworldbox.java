49fe7eb5753602e3a583b31714a43a485e346458
Nathaniel Payne <njpayne@sfu.ca>
2013-09-14 00:50:35 -0700

    Pop out dialogue box

---
+++ helloworldbox.java
@@ -0,0 +1,12 @@
+/* 
+This application displays Hello World! in a comment box in Java
+*/
+
+import javax.swing.JOptionPane; // Import thte JOptionPane class
+
+public class helloworldbox {
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "Hello World!");
+		//System.out.println("Hello World!"); // Previous method
+	}
+}
