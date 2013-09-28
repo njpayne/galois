6d9ddfabce2c1044de49c56bd052512246404f59
Nathaniel Payne <njpayne@sfu.ca>
2013-09-27 21:26:40 -0700

    Demonstrating three messages output in Java

---
+++ introduction/three_messages.java
@@ -0,0 +1,7 @@
+public class three_messages {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Programming is fun");
+  }
+}
