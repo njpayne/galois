c24b7b679cdd1abf161446715714c488b5488576
Nathaniel Payne <njpayne@sfu.ca>
2013-09-28 23:21:16 -0700

    Five messages repeated

---
+++ introduction/welcome_five.java
@@ -0,0 +1,9 @@
+public class welcome_five {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+  }
+}
