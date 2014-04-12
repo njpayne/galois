b6623c7ff12209cb9e4348ad2c489f64334982d4
Nathaniel Payne <npayne@cardinalpath.com>
2014-04-11 21:13:34 -0700

    Adding a new rock, paper, scissors Java game

--- selections/src/selections_programming/rock_paper_scissors.java
+++ selections/src/selections_programming/rock_paper_scissors.java
@@ -21,38 +21,102 @@ public class rock_paper_scissors {
 		System.out.println("Good morning and welcome to the rock-paper-scissors game!");
 		System.out.println("This came will give you the chance to battle me, the computer, in rock, paper, scissors!");
 		System.out.println("To start playing the game, please select 0 for a scissor, 1 for a rock, and 2 for paper:");
-		System.out.println("To exit the system, please enter the number three");
+		System.out.println("To exit the system, please enter the number 3.");
 		System.out.println("Please enter your solution now:");
 		
 		// Create a simple while loop to roll through the answers
 		int entry = input.nextInt(); // Grab the input from the user
 		
+		// Create a simple variable that marks the rock as a rock ... 
+		String a = "rock"; // 1 refers to a rock
+		String b = "paper"; // 2 refers to paper
+		String c = "scissor"; // 0 refers to a scissor
+		
 		while(entry != 3) { // Note that this generates an infinite loop versus the do while loop
 			// Note that the do while loop evaluates the expression at the top of the loop
-			
+						
 			// Generate a random number in order to roll through the relevant answers
 			int random_number = randomGenerator.nextInt(3); // Generate a random integer between 0 and 3 
 			
-			// Grab the users random number			
-			if(entry == 0){ // This is a scissor (scissor beats paper; rock beats scissor, tie)					
+			// Grab the users entered number			
+			if(entry == 0){ // This is a scissor (scissor beats paper; rock beats scissor, tie)
+				if(random_number == 2){
+					// The user has paper and the random number is a scissor; scissor beats paper
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected " + c + " and I selected a " + b + "! This means that I win!");
+				}
+				
+				else if(random_number == 1){
+					// The user has paper and the random number is a rock; paper beats rock
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected " + c + " and I selected a " + a + "! You win!");
+				}
+				
+				else{
+					// We have a tie
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected " + b + " and I selected " + b + "! We have a tie!");					
+				}
 										
 				}
 			
 			else if (entry == 1){ // This is a rock (rock beats scissor, paper beats rock, tie)
+				if(random_number == 2){
+					// The user has a rock and the random number is a scissor; scissor beats paper
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected a " + a + " and I selected" + b + "! This means that I win!");
+				}
 				
-			} // Review
-			
-			else if (entry == 2){ // This is a piece of paper (paper beats rock, scissor beats paper, tie)
+				else if(random_number == 0){
+					// The user has a rock and the random number is a rock; paper beats rock
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected a " + a + " and I selected a " + c + "! You win!");
+				}
+				
+				else{
+					// We have a tie
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected a " + a + " and I selected a " + a + "! We have a tie!");					
+				}
 				
 			}
 			
-		}
+			else if (entry == 2){ // This is a piece of paper (paper beats rock, scissor beats paper, tie)
+				if(random_number == 0){
+					// The user has paper and the random number is a scissor; scissor beats paper
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected " + b + " and I selected a " + c + "! This means that I win!");
+				}
+				
+				else if(random_number == 1){
+					// The user has paper and the random number is a rock; paper beats rock
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected " + b + " and I selected a " + a + "! You win!");
+				}
+				
+				else{
+					// We have a tie
+					// System.out.println("Thank you for playing the game!");
+					System.out.println("You selected " + b + " and I selected " + b + "! We have a tie!");					
+				}
+			}
+			
+			else{
+				System.out.println("I am sorry to learn that you cannot type!");
+				System.out.println("You selected an option that was neither a 1, 2, or 3!");
+			}
+			
+			System.out.println("");
+			System.out.println("To re-start a new game, please select 0 for a scissor, 1 for a rock, and 2 for paper:");
+			System.out.println("Otherwise, to exit and stop the fun, please enter the number 3!");
+			System.out.println("Please enter your solution now:");
+			
+			// Create a simple while loop to roll through the answers
+			entry = input.nextInt(); // Grab the new input from the user
+			
+		} // End while loop
 		
-		// While the solution is not 3
-		
-		// Ask the user for input
-		
-		// Run through the conditional if statements & return the value to the user of the relevant output
-		
-	}
-}
+	System.out.println("We are very sorry to see you go! Please come back when you have had more practice!");
+	
+	} // End the public static void
+} // End public class
