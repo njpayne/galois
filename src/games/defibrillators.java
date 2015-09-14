d88a328a99e5a733fb04783e955bff8d8225e425
Nathaniel Payne <npayne@cardinalpath.com>
2015-09-14 13:30:11 -0700

    Adding a new partially completed big data search program

---
+++ src/games/defibrillators.java
@@ -0,0 +1,28 @@
+package games;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Created by nathanielpayne on 9/14/2015.
+ */
+public class defibrillators {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String lON = in.next();
+        in.nextLine();
+        String lAT = in.next();
+        in.nextLine();
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String dEFIB = in.nextLine();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
